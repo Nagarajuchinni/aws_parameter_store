08ef74e698de1ba34e17ece68ee65567ea47f029
Nagarajuchinni <nagarajchinni3@gmail.com>
2024-11-08 00:16:55 +0530

    test

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
-Hi
+terraform {
+  backend "s3" {
+    bucket = "d76-terraform-state-chinni"
+    key    = "parameter-store/terraform.tfstate"
+    region = "us-east-1"
+
+  }
+}
+
+variable "parameters" {
+    default = [
+        { name = "test", value = "string", value = "test"},
+        { name = "test1", value = "string", value = "test1"}
+    ]
+      
+    
+}
+
+resource "aws_ssm_parameter" "params" {
+    count = length(var.parameters)
+    name = var.parameters[count.index]
+    type = var.parameters[count.index].type
+    value = var.parameters[count.index].value
+}
+    
