8be808eefb884f56a563accde9a5ecc415c8f174
Nagarajuchinni <nagarajchinni3@gmail.com>
2024-11-08 19:18:55 +0530

    test

--- vars.tf
+++ vars.tf
@@ -1,10 +1,10 @@
-variable "parameters" {
-    default = [
-        { name = "ses.username", type = "String", value = "AKIAZ55NM"},
-        { name = "dev.rds.username", type = "String", value = "admin1"},
+# variable "parameters" {
+#     default = [
+#         { name = "ses.username", type = "String", value = "AKIAZ55NM"},
+#         { name = "dev.rds.username", type = "String", value = "admin1"},
 
-        ##pssword##
-        { name = "dev.rds.password", type = "SecureString", value = "expense123"},
-    ]     
+#         ##pssword##
+#         { name = "dev.rds.password", type = "SecureString", value = "expense123"},
+#     ]     
     
-}
+# }
