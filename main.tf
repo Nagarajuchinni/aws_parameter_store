b27381ed647e1b02665dfe6d11cc7171baaff0b2
Nagarajuchinni <nagarajchinni3@gmail.com>
2024-11-08 19:16:41 +0530

    test

--- main.tf
+++ main.tf
@@ -7,11 +7,18 @@ terraform {
   }
 }
 
+variable "parameters" {
+    default = [
+        { name = "test", type = "String", value = "test"},
+        { name = "test1", type = "String", value = "test1"}
+    ]     
+    
+}
 
 
 resource "aws_ssm_parameter" "params" {
     count = length(var.parameters)
-    name = var.parameters[count.index]
+    name = var.parameters[count.index].name
     type = var.parameters[count.index].type
     value = var.parameters[count.index].value
 }
